9d993adfff9e9c723a8345d4aa5b6ebfda7211c0
aaron <202780181@qq.com>
2022-09-14 22:20:50 +0800

    fix: add less and remove prisma

---
+++ packages/web/app/routes/sign/in/index.tsx
@@ -0,0 +1,8 @@
+import style from './style.less'
+export default function signIn() {
+  return (
+    <div className={style.app}>
+      这是首页内容
+    </div>
+  );
+}
--- packages/web/remix.env.d.ts
+++ packages/web/remix.env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
+
+declare module '*.less' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
