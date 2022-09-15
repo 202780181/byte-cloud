1435de374b30fa619c2ee65c272dc312e6dc0e32
aaron <202780181@qq.com>
2022-09-15 22:32:12 +0800

    feat: update

--- packages/web/app/routes/sign/in/index.tsx
+++ packages/web/app/routes/sign/in/index.tsx
@@ -4,7 +4,7 @@ export default function signIn() {
   return (
     <div className="home" data-with-download="true">
       <div className="download">
-        <img src="https://oss.yun1024.com/website/7001-2-tps-2186-1376.png?Expires=1663238339&OSSAccessKeyId=TMP.3KhGyvvLxZbC2K4osdKPvNYDBBg5a2qHUrK3DMkhXwyhGAjUUeKHoMQJfBopRC69kUwLTt6VmCefM8DLphuxqiUsHfv3o6&Signature=1EY%2FOksKizaVGSEJzsT7npkMA9E%3D"
+        <img src="https://oss.yun1024.com/website/7001-2-tps-2186-1376.png"
              className="desktop-img"/>
         <div className="desc">下载桌面端，上传下载更方便</div>
         <a  href="https://yunpan.aliyun.com/downloads/apps/desktop/aDrive.dmg" className="download-action">
@@ -14,10 +14,14 @@ export default function signIn() {
         </a>
       </div>
       <div className="login">
-        <img data-v-3e061979="" src="https://gw.alicdn.com/imgextra/i2/O1CN012zI3pB1XLS21rJ9Je_!!6000000002907-55-tps-108-24.svg" width="216"/>
+        <img src="https://gw.alicdn.com/imgextra/i2/O1CN012zI3pB1XLS21rJ9Je_!!6000000002907-55-tps-108-24.svg" width="216"/>
         <div className="main">
           <div className="login-frame">
+            <div id='login' className="width-vertical login-label-text login-view-sms web">
+              <div className="login-content nc-outer-box">
 
+              </div>
+            </div>
           </div>
         </div>
       </div>
