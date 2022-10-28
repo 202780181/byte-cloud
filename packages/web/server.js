245c390dd3a29214955463c909747a3f142b86df
longyadong <202780181@qq.com>
2022-10-28 11:04:17 +0800

    fix: update

--- packages/web/server.js
+++ packages/web/server.js
@@ -1,4 +1,4 @@
 const { createRequestHandler } = require("@remix-run/node")
 import * as build from "@remix-run/dev/server-build";
 
-export const app = createRequestHandler({build, mode: process.env.NODE_ENV})
+module.exports = createRequestHandler({build, mode: process.env.NODE_ENV})
